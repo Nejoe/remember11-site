958dc8c026a406b9e9d2633e8d8fb51c323cf802
Nejoe <981591613@qq.com>
2024-03-11 00:29:54 +0800

    添加文章《Self在哪里？》

--- docs/.vuepress/.temp/blog/type.js
+++ docs/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[3,2,0,1]}},"timeline":{"/":{"path":"/timeline/","indexes":[3,2,0,1]}}};
+      export const typesMap = {"article":{"/":{"path":"/article/","indexes":[0,5,1,4,2,3]}},"timeline":{"/":{"path":"/timeline/","indexes":[0,5,1,4,2,3]}}};
       
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
