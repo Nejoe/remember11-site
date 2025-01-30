605939623a81feb206b73bad2b9762356665b3c9
Nejoe <981591613@qq.com>
2025-01-30 22:15:16 +0800

    评论数据库已变更，去掉科学上网提示

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -5,16 +5,7 @@ import ParentLayout from "@vuepress/theme-default/layouts/Layout.vue";
 <template>
   <ParentLayout>
     <template #page-bottom>
-      <div class="my-footer">
-        <em>由于数据库限制，评论功能需要科学上网使用。</em>
-      </div>
       <CommentService />
     </template>
   </ParentLayout>
 </template>
-
-<style lang="css">
-.my-footer {
-  text-align: center;
-}
-</style>
